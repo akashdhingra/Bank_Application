fab5d89d948662fa177cb68ed3459f035cc0873f
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-15 19:40:01 -0400

    set architecture for accounts class

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,2 +1,8 @@
 public abstract class Accounts implements IBaseRate {
+
+    // List common properties for savings and checking account
+
+    // Constructor to set base properties and initialize the account
+
+    // List common methods
 }
