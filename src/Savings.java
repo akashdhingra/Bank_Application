f4e43d075cdaddc50ba6defc57042cd47d3e5566
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-17 18:41:29 -0400

    implemented savings account features

--- src/Savings.java
+++ src/Savings.java
@@ -8,12 +8,21 @@ public class Savings extends Accounts {
     public Savings(String name, String SSN, float deposit){
         super(name, SSN, deposit);
         accountNumber = "1" + accountNumber;
+        setSafetyDepositInfo();
+    }
+    private void setSafetyDepositInfo()
+    {
+        SafetyDepositBoxID = (int)(Math.random() * Math.pow(10,3));
+        SafetyDepositBoxKey = (int)(Math.random() * Math.pow(10,4));
     }
 
     // List any methods specific for saving account
     public void showInfo()
     {
         super.showInfo();
-        System.out.println("Account type : Savings Account");
+        System.out.println("Savings Account feature- " +
+                "\nSafety Deposit Box ID: " + SafetyDepositBoxID +
+                "\nSafety Deposit Box Key: " + SafetyDepositBoxKey
+                );
     }
 }
