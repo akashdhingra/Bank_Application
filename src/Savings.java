770d10ef15c5f7b414289461065f3d585c91e14b
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-17 11:58:48 -0400

    set architecture for savings class

--- src/Savings.java
+++ src/Savings.java
@@ -1,2 +1,8 @@
 public class Savings extends Accounts {
+
+    // List properties specific for savings account
+
+    // Constructor to initialize settings for the savings properties
+
+    // List any methods specific for saving account
 }
