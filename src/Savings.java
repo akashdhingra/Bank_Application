41ac563c4e6c64042725bed0c401a2c120115e14
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-17 12:20:20 -0400

    common properties of savings account

--- src/Savings.java
+++ src/Savings.java
@@ -1,6 +1,8 @@
 public class Savings extends Accounts {
 
     // List properties specific for savings account
+    int SafetyDepositBoxID;
+    int SafetyDepositBoxKey;
 
     // Constructor to initialize settings for the savings properties
     public Savings(String name){
