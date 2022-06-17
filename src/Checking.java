a697b092c26d3b0720bdc55ff031f3dcaa0a9f55
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-17 12:19:34 -0400

    common properties of checking account

--- src/Checking.java
+++ src/Checking.java
@@ -1,5 +1,7 @@
 public class Checking extends Accounts{
     // List properties specific for checking account
+    int DebitCardNumber;
+    int DebitCardPin;
 
     // Constructor to initialize settings for the checking properties
     public Checking(String name){
