b8a9378ab467b984cb246955fc5046c23276e7f7
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-17 12:18:45 -0400

    common properties of accounts

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,6 +1,11 @@
 public abstract class Accounts implements IBaseRate {
 
     // List common properties for savings and checking account
+    String name;
+    String SocialSecurityNumber;
+    String accountNumber;
+    double balance;
+    double rate;
 
     // Constructor to set base properties and initialize the account
     public Accounts(String Name)
