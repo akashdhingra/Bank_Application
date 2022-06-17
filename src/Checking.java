4abd5ec481d40129a4301cfbbd6e101e23432b04
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-17 11:59:15 -0400

    set architecture for checking class

--- src/Checking.java
+++ src/Checking.java
@@ -1,2 +1,7 @@
 public class Checking extends Accounts{
+    // List properties specific for checking account
+
+    // Constructor to initialize settings for the checking properties
+
+    // List any methods specific for checking account
 }
