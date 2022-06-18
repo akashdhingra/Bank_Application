0a7fa8059d8f4b01b7127990138a622acde0fcb5
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-18 17:04:05 -0400

    read CSV file

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,11 +1,8 @@
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args) {
-        Checking checkacc1 = new Checking("Akash","123456789",1200);
-        Savings saveacc1 = new Savings("Dhingra","987654321", 800);
 
-        checkacc1.showInfo();
-        System.out.println("***************");
-        saveacc1.showInfo();
-        saveacc1.CompoundInterest();
+
     }
 }
